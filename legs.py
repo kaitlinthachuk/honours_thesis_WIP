ce12eb61c079eed7abcacbccb5aa5dbb5626f212
Kaitlin Thachuk <kaitlinthachuk@gmail.com>
2017-11-14 20:32:19 -0800

    Completed ragdoll test

--- legs.py
+++ legs.py
@@ -5,7 +5,11 @@ physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
 p.setAdditionalSearchPath(pybullet_data.getDataPath()) #used by loadURDF
 planeId = p.loadURDF("plane.urdf")
 p.setGravity(0,0,-10)
-legsStartPos = [0,0,0.3]
-legsStartOrientation = p.getQuaternionFromEuler([0,0,0])
+legsStartPos = [0,0,3]
+legsStartOrientation = p.getQuaternionFromEuler([0.2,0.3,1])
 legsID = p.loadURDF("legs.urdf", legsStartPos,legsStartOrientation)
-#p.setRealTimeSimulation(1)
+numJoints = p.getNumJoints(legsID)
+maxForce = [0] * numJoints
+mode = p.VELOCITY_CONTROL
+p.setJointMotorControlArray(legsID, range(0, numJoints), controlMode=mode, forces=maxForce)
+p.setRealTimeSimulation(1)
