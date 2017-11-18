6c8963b07e28fa74152d8f0b7f1271d479f76185
Kaitlin Thachuk <kaitlinthachuk@gmail.com>
2017-11-17 16:30:21 -0800

    Fixed joint origins so joints hinge around correct point, giving more natural looking movement

--- legs.py
+++ legs.py
@@ -5,8 +5,8 @@ physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
 p.setAdditionalSearchPath(pybullet_data.getDataPath()) #used by loadURDF
 planeId = p.loadURDF("plane.urdf")
 p.setGravity(0,0,-10)
-legsStartPos = [0,0,3]
-legsStartOrientation = p.getQuaternionFromEuler([0.2,0.3,1])
+legsStartPos = [0,0,2]
+legsStartOrientation = p.getQuaternionFromEuler([0,0,0])
 legsID = p.loadURDF("legs.urdf", legsStartPos,legsStartOrientation)
 numJoints = p.getNumJoints(legsID)
 maxForce = [0] * numJoints
